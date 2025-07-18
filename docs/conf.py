915521293380934fe935df4a954d7af4b837e2e7
ajcecil <75514611+ajcecil@users.noreply.github.com>
2025-07-18 13:16:38 -0500

    enw

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(".."))  # so Sphinx can find your package
 
-html_logo = "_static/GLSI_logo.png"
+
 project = "GLSI Tools"
 author = "GLSI"
 release = "0.0.0"
@@ -33,3 +33,4 @@ myst_enable_extensions = [
 # HTML output options
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_logo = "_static/GLSI_logo.png"
